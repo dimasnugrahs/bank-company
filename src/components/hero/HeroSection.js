f0a7f6bfdee34415f16bf910521097f79a526076
dimasnugrahs <79310230+dimasnugrahs@users.noreply.github.com>
2024-10-27 16:56:50 +0800

    update background hero section

--- src/components/hero/HeroSection.js
+++ src/components/hero/HeroSection.js
@@ -3,7 +3,7 @@ import background from "../../assets/images/background.png";
 
 const HeroSection = () => {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-gray-900">
       <div className="grid max-w-screen-xl px-4 py-8 xl:py-36 xl:pt-44 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7 text-center lg:text-left pt-48 pb-48 lg:pt-0 lg:pb-0">
           <h1 className="max-w-2xl mb-8 text-4xl font-extrabold tracking-tight leading-none  md:text-5xl xl:text-6xl dark:text-white">
