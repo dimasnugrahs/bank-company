d3d3cda01aba81e77a5df4d111d2da76345ea9d7
dimasnugrahs <79310230+dimasnugrahs@users.noreply.github.com>
2024-10-30 01:22:52 +0800

    update logo navigation

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,7 @@
 // Navbar.js
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import iconCompany from "../../assets/images/icon-company.png";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -58,7 +59,11 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto">
         <div className="flex items-center justify-between py-4 md:py-0">
           <div className="flex-shrink-0">
-            <h1 className="text-white font-bold">Logo</h1>
+            <img
+              src={iconCompany}
+              alt="Icon"
+              className="max-w-8 border-white border rounded-full"
+            ></img>
           </div>
 
           <div className="hidden md:flex flex-grow justify-center space-x-4 ">
